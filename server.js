148dff5e33bb5fe962b5de24d6b54e4b243be7fd
Ashfaq Farleen <139785309+devmaniac1@users.noreply.github.com>
2024-03-01 14:24:58 +0530

    Connect DataBAse

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
-const app = express();
-const { Client } = require("@googlemaps/google-maps-services-js");
+const mongoose = require("mongoose");
 const fetch = require("node-fetch");
-const googleMapsClient = new Client();
+const dotenv = require("dotenv");
+const { Client } = require("@googlemaps/google-maps-services-js");
 
+const app = express();
+dotenv.config({ path: "./config.env" });
+
+const mongoDB = `mongodb+srv://lankanamigo:${process.env.DATABASE_PASSWORD}@cluster.2yzcprp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`;
+
+const googleMapsClient = new Client();
 app.use(express.json());
 
 app.get("/calculate-route", async (req, res) => {
   // const {fromLocation,toLocation,travelMode} = req.query;
   const fromLocation = "Galle, Sri Lanka";
   const toLocation = "Ella, Sri Lanka";
-  const apiKey = "AIzaSyBkePZHNAeceiSPlP4LuZIPd28NpBJcaF8"; // Replace with your actual API key
+  const apiKey = process.env.GOOGLEAPI;
   try {
     const directionsResponse = await googleMapsClient.directions({
       params: {
@@ -30,36 +36,55 @@ app.get("/calculate-route", async (req, res) => {
 });
 
 app.get("/get-accomodations", async (req, res) => {
-  // const {latLong} = req.query;
-  const apiKey = "FDE9409345FD4A24B21D4A6CE6D20369";
-  const latLong = "6.8667%2C81.0466";
-  const url = `https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=${latLong}&key=FDE9409345FD4A24B21D4A6CE6D20369&category=hotels&radius=3&radiusUnit=km&language=en`;
-  const options = { method: "GET", headers: { accept: "application/json" } };
+  try {
+    const apiKey = process.env.TRIPADVISORAPI;
+    const latLong = "6.9497%2C80.7891";
+    const url = `https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=${latLong}&key=${apiKey}&category=hotels&radius=3&radiusUnit=km&language=en`;
+    const options = { method: "GET", headers: { accept: "application/json" } };
 
-  fetch(url, options)
-    .then((res) => res.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.error("error:" + err));
+    const response = await fetch(url, options);
+    const jsonData = await response.json();
+
+    // const accommodations = jsonData.data.map((data) => ({
+    //   locationId: data.location_id,
+    //   name: data.name,
+    //   distance: data.distance,
+    //   city: data.address_obj.city,
+    //   address: data.address_obj.address_string,
+    // }));
+
+    // await Accommodation.insertMany(accommodations);
+    
+
+    res.json(jsonData);
+  } catch (error) {
+    console.error("Error fetching accommodations:", error);
+    res.status(500).json({ error: "Failed to fetch accommodations" });
+  }
 });
 
 app.get("/get-accomodations-details", async (req, res) => {
   // const {locationId} = req.query;
   const locationId = "19847553";
+  const apiKey = process.env.TRIPADVISORAPI;
 
-  const url = `https://api.content.tripadvisor.com/api/v1/location/${locationId}/details?key=FDE9409345FD4A24B21D4A6CE6D20369&language=en&currency=USD`;
+  const url = `https://api.content.tripadvisor.com/api/v1/location/${locationId}/details?key=${apiKey}&language=en&currency=USD`;
   const options = { method: "GET", headers: { accept: "application/json" } };
 
-  fetch(url, options)
-    .then((res) => res.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.error("error:" + err));
+  const response = await fetch(url, options);
+  const jsonData = await response.json();
+
+ 
+  res.json(jsonData);
+
+ 
 });
 
 app.get("/get-accomodations-photo", async (req, res) => {
   // const {locationId} = req.query;
   const locationId = "19847553";
-
-  const url = `https://api.content.tripadvisor.com/api/v1/location/${locationId}/photos?key=FDE9409345FD4A24B21D4A6CE6D20369&language=en&limit=5`;
+  const apiKey = process.env.TRIPADVISORAPI;
+  const url = `https://api.content.tripadvisor.com/api/v1/location/${locationId}/photos?key=${apiKey}&language=en&limit=5`;
   const options = { method: "GET", headers: { accept: "application/json" } };
 
   fetch(url, options)
@@ -68,20 +93,19 @@ app.get("/get-accomodations-photo", async (req, res) => {
     .catch((err) => console.error("error:" + err));
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-app.get("/nearby", async (req, res) => {
-  // const {locationId} = req.query;
-  const locationId = "19847553";
+const PORT = process.env.PORT || 3002;
 
-  const url =
-    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=hotel&location=-33.8670522%2C151.1957362&radius=1500&type=hotel&key=AIzaSyBkePZHNAeceiSPlP4LuZIPd28NpBJcaF8";
-  const options = { method: "GET", headers: { accept: "application/json" } };
-
-  fetch(url, options)
-    .then((res) => res.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.error("error:" + err));
-});
+mongoose
+  .connect(mongoDB)
+  .then(() => {
+    const connection = mongoose.connection;
+    connection.once("open", () => {
+      console.log("connected");
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
