321efe8d8ca3ff5eda3293cb02743412e9a87db7
Saleem Malik <roxsaleemmalik1@gmail.com>
2024-03-12 13:51:35 +0530

    Firebase Deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-
+const functions = require("firebase-functions");
 const userRouter = require("./routes/userRoutes");
 const googleRouter = require("./routes/googleRoutes");
 const tripadvisorRouter = require("./routes/tripAdvisorRoutes");
@@ -11,12 +11,13 @@ const serpRouter = require("./routes/serpRoutes");
 
 const app = express();
 
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
 dotenv.config({ path: "./config.env" });
+
+const PORT = process.env.PORT || 3002;
 
 const mongoDB = `mongodb+srv://lankanamigo:${process.env.DATABASE_PASSWORD}@cluster.2yzcprp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`;
 
@@ -24,8 +25,13 @@ app.use("/api/users", userRouter);
 app.use("/googleAPI", googleRouter);
 app.use("/tripadvisorAPI", tripadvisorRouter);
 app.use("/serpAPI", serpRouter);
-
-const PORT = process.env.PORT || 3002;
+app.get("/", (req, res) => {
+  res.json({
+    status: process.env.GOOGLEAPI,
+    port: process.env.PORT,
+    portD: PORT,
+  });
+});
 
 mongoose
   .connect(mongoDB)
@@ -41,3 +47,5 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+exports.api = functions.https.onRequest(app);
